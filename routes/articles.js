4ce24858362985c6a8feb127b6d943be2851b14f
sharmaaman4419 <sharmaaman4419@gmail.com>
2019-08-25 12:09:46 +0530

    handled-update-route

--- routes/articles.js
+++ routes/articles.js
@@ -3,10 +3,10 @@ var Article = require("../models/article")
 //requring the express
 var express = require("express")
 //acessing the routes
-var route = express.Router();
+var router = express.Router();
 
 //handling the route post request
-route.post("/",(req,res) => {
+router.post("/",(req,res) => {
     // console.log("Post the data");
     Article.create(req.body,(err,createduser) => {
         if (err) console.log("error")
@@ -16,7 +16,7 @@ route.post("/",(req,res) => {
 })
 
 //handling the get routeS
-route.get("/",(req,res) => {
+router.get("/",(req,res) => {
     Article.find({},(err,connected) => {
         console.log(connected);
         if(err) console.log("err")
@@ -24,12 +24,20 @@ route.get("/",(req,res) => {
     })
 })
 //handling the delete route
-route.delete("/:id",(req,res) => {
+router.delete("/:id",(req,res) => {
     var id = req.params.id;
     Article.findByIdAndDelete(id, (err,handled) => {
         if(err) console.log("error");
         res.redirect('/article');
     })
 })
+//handling the update route
+router.put("/:id",(req,res) => {
+   var id = req.params.id;
+   Article.findByIdAndUpdate(id,(err,updated) => {
+       if(err) console.log("error in updating")
+       res.redirect("/article")
+    })
+})
 //exporting the route
-module.exports = route;
+module.exports = router;
